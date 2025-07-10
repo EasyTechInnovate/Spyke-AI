0674f4c1b5f803ad5c1e736086c7b303df2fa3d4
manishdashsharma <mdashsharma95@gmail.com>
2025-07-11 00:53:22 +0530

    fix

--- client/app/page.jsx
+++ client/app/page.jsx
@@ -1,12 +1,4 @@
-"use client"
-export const dynamic = 'force-dynamic';
-
-import CuratedUseCases from "@/components/landing/CuratedUseCases"
-import FeatureCards from "@/components/landing/FeaturedCollection"
-import FeaturedCollections from "@/components/landing/FeaturedCollection"
-import FeaturedPrompts from "@/components/landing/FeaturePrompt"
 import HeroSection from "@/components/landing/HeroSection"
-import QuickFilters from "@/components/landing/QuickFilters"
 import Footer from "@/components/layout/Footer"
 import Header from "@/components/layout/Header"
 
